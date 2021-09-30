01c44d4b07da48bc331dd5bce0a9bc04fdc7b26b
Vera Leitao <61667405+veraleitaodev@users.noreply.github.com>
2021-09-30 12:27:45 +0100

    added content to home.scss

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60520" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64301" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
